44eabd081b149262d24706cb306209f658220d9a
Gabriel <gabriel.mgran20@hotmail.com>
2022-01-21 03:35:58 -0300

    update

---
+++ olamundo.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang=pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando PHP</title>
+</head>
+   <style>
+       h1 {
+           background-color: aqua;
+           text-align: center;
+           color: darkgray;
+           box-shadow: 3px 3px 3px 0px black;
+           text-shadow:1px 1px 1px black;
+
+       }
+
+       h2 {
+           background-color: brown;
+           text-align: center;
+           color: darkgray;
+           box-shadow: 3px 3px 3px 0px black ;
+           text-shadow:1px 1px 1px black;
+
+
+       }
+
+
+
+
+
+   </style>
+<body>
+    <h1>Testando</h1>
+
+
+    <?php 
+
+    echo "<h2>Olá, <br> mundo!</h2>";
+
+
+    
+    
+    
+    
+    
+    
+    
+    ?>
+
+
+
+
+    
+</body>
+</html>
