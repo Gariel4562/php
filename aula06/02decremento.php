8c7ad7087d4d330b5370a36473a94be0e32b57f4
Gabriel <gabriel.mgran20@hotmail.com>
2022-02-11 00:56:34 -0300

    update

--- aula06/02decremento.php
+++ aula06/02decremento.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>incremento</title>
+    <title>Decremento</title>
 </head>
 <body>
     <?php
     $atual = $_GET["aa"];
-    echo "o Ano atual é $atual";
+    echo "o Ano atual é $atual e o ano anterior é " . --$atual;
 
 
 
