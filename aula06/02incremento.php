ba2572bb6d2065266b873baba7b5edb3ef6e8133
Gabriel <gabriel.mgran20@hotmail.com>
2022-02-11 00:50:18 -0300

    update

---
+++ aula06/02incremento.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>incremento</title>
+</head>
+<body>
+    <?php
+    $atual = $_GET["aa"];
+    echo "o Ano atual é $atual e o ano anterior é " . --$atual;
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+    
+</body>
+</html>
