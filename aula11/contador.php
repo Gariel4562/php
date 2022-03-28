39e98f84176dd855a1ef6da4a5173da3c2a73014
Gabriel <gabriel.mgran20@hotmail.com>
2022-03-28 15:16:07 -0300

    update

---
+++ aula11/contador.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo contador while</title>
+    <link rel="stylesheet" href="estilo/style.css">
+</head>
+<body>
+    <h1>Exemplo contador while</h1>
+    <?php
+    $c = 1;
+
+    while($c <= 15) {
+        echo "$c<br>";
+        $c++;
+    }
+
+
+
+
+
+
+
+
+   
+
+    ?>
+
+
+
+
+
+
+
+    
+</body>
+</html>
