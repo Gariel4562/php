5729d2283a630eefd7f3425bba5a93b6953f5abd
Gabriel <gabriel.mgran20@hotmail.com>
2022-05-05 15:56:58 -0300

    update

---
+++ aula16/exercicio04.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>função str_word_count</title>
+    <link rel="stylesheet" href="estilo/style.css">
+</head>
+<body>
+    <?php
+         
+         $frase = "Eu vou estudar php agora";
+         $cont = str_word_count($frase,2); // posso colocar 0, 1, 2
+         print_r($cont);
+
+
+
+
+
+
+
+
+
+
+    ?>
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ aula16/exercicio06.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHR</title>
+    <link rel="stylesheet" href="estilo/style.css">
+
+
+
+</head>
+<body>
+    <?php
+         $letra = chr(99);
+         echo "A letra de código 99 é $letra."
+
+
+
+
+
+
+    ?>
+
+
+
+    
+</body>
+</html>
